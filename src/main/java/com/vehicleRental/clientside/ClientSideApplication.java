792e3cf49c5446dad01d34baba98caf6b6cdd292
ced <cedric.milius@yahoo.fr>
2019-11-21 17:28:58 +0100

    add eureka

--- src/main/java/com/vehicleRental/clientside/ClientSideApplication.java
+++ src/main/java/com/vehicleRental/clientside/ClientSideApplication.java
@@ -2,8 +2,12 @@ package com.vehicleRental.clientside;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableEurekaClient
 public class ClientSideApplication {
 
 	public static void main(String[] args) {
